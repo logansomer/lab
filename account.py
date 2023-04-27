52cd963c1937ae75e3e8faf32039bea57368e09c
logansomer <loganjsomerville@gmail.com>
2023-04-27 13:48:26 -0500

    added doc strings and hinting

--- account.py
+++ account.py
@@ -1,20 +1,44 @@
 class Account:
-    def __init__(self,name):
-        __account_balance = 0
-        __account_name = name
-    def deposit(self,amount):
-        if amount <= 0:
+    def __init__(self,name)-> None:
+        '''
+        Funtion sets up inital objects
+        :param name: name of user
+        '''
+        self.__account_balance = 0.00
+        self.__account_name = name
+    def deposit(self,amount:float) -> float:
+        '''
+        Funtion allows user to depsit sed amount into account balance
+        :param amount: valuble for inputed number value as float
+        :return: True or False (depending on input)
+        '''
+        if amount <= 0.00:
             return False
         else:
             self.__account_balance += amount
             return True
-    def withdraw(self,amount):
-        if amount <= 0 or amount > self.__account_balance:
+
+    def withdraw(self,amount:float) -> float:
+        '''
+        Funtion allows user to withdraw sed amount from account balance
+        :param amount: valuble for inputed number value as float
+        :return: True or False (depending on input)
+        '''
+        if amount <= 0.00 or amount > self.__account_balance:
             return False
         else:
             self.__account_balance -= amount
             return True
-    def get_balance(self):
+    def get_balance(self) -> float:
+        '''
+        fution returns balance (helps clean up code)
+        :return: account balance
+        '''
         return self.__account_balance
-    def get_name(self):
+    def get_name(self) -> str:
+        '''
+        fution returns name (helps clean up code)
+        :return: account name
+        '''
         return self.__account_name
+
--- test_account.py
+++ test_account.py
@@ -23,12 +23,18 @@ class Test:
     def test_withdraw(self):
         assert self.name1.withdraw(0) is False
         assert self.name1.get_balance() == 0.00
-        assert self.name1.withdraw(-1) is False
+
+        assert self.name1.withdraw(5) is False
         assert self.name1.get_balance() == 0.00
-        assert self.name1.deposit(3) is True
+
+        self.name1.deposit(3)
         assert self.name1.get_balance() == 3.00
-        assert self.name1.withdraw(2) is True
-        assert self.name1.get_balance() == 1.00
+
+        assert self.name1.withdraw(2.5) is True
+        assert self.name1.get_balance() == approx(0.5, abs=0.001)
+
+
+
 
 
 
