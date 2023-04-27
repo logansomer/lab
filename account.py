5128a9a396376749d97a6f7676dd7c08c9dec7bb
logansomer <loganjsomerville@gmail.com>
2023-04-27 13:52:19 -0500

    added doc strings and hinting

--- account.py
+++ account.py
@@ -1,15 +1,15 @@
 class Account:
-    def __init__(self,name:str)-> None:
+    def __init__(self,name)-> None:
         '''
         Funtion sets up inital objects
-        :param name:
+        :param name: name of user
         '''
         self.__account_balance = 0.00
         self.__account_name = name
     def deposit(self,amount:float) -> float:
         '''
         Funtion allows user to depsit sed amount into account balance
-        :param amount:
+        :param amount: valuble for inputed number value as float
         :return: True or False (depending on input)
         '''
         if amount <= 0.00:
@@ -20,8 +20,8 @@ class Account:
 
     def withdraw(self,amount:float) -> float:
         '''
-        Funtion allows yser to withdraw sed amount from account balance
-        :param amount:
+        Funtion allows user to withdraw sed amount from account balance
+        :param amount: valuble for inputed number value as float
         :return: True or False (depending on input)
         '''
         if amount <= 0.00 or amount > self.__account_balance:
@@ -29,13 +29,13 @@ class Account:
         else:
             self.__account_balance -= amount
             return True
-    def get_balance(self:float) -> float:
+    def get_balance(self) -> float:
         '''
         fution returns balance (helps clean up code)
         :return: account balance
         '''
         return self.__account_balance
-    def get_name(self:str) -> str:
+    def get_name(self) -> str:
         '''
         fution returns name (helps clean up code)
         :return: account name
